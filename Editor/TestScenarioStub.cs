730a6f4b8f7e0c5ab7481d1017a4e2e80a586396
Eideren <contact@eideren.com>
2023-07-13 21:25:40 +0200

    Tester: noop when testing scene bound commands, destroy tester object after running the whole stage

--- Editor/TestScenarioStub.cs
+++ Editor/TestScenarioStub.cs
@@ -1,4 +1,7 @@
-﻿using UnityEditor;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace Screenplay.Editor
@@ -8,18 +11,54 @@ namespace Screenplay.Editor
         public Scenario Scenario;
         void OnGUI()
         {
-            if (EditorApplication.isPlaying)
+            if (!EditorApplication.isPlaying)
+                return;
+            Close();
+
+            var scenarioInScene = new GameObject(nameof(TestScenarioStub)).AddComponent<ScenarioTesterComp>();
+            scenarioInScene.Scenario = Scenario;
+            var overrides = new List<BindingOverride>();
+            foreach (var binding in Scenario.Bindings)
             {
-                var scenarioInScene = new GameObject(nameof(TestScenarioStub)).AddComponent<ScenarioTesterComp>();
-                scenarioInScene.Scenario = Scenario;
-                new Stage(scenarioInScene);
-                Close();
+                if (binding.IsSceneBound)
+                {
+                    overrides.Add(new()
+                    {
+                        Name = binding.Name,
+                        Command = new DoNothing()
+                    });
+                }
+            }
+
+            if (overrides.Count > 0)
+                Debug.LogWarning($"Commands {string.Join(", ", overrides.Select(x => x.Name))} were not set, we'll ignore them");
+
+            scenarioInScene.Overrides = overrides.ToArray();
+            var stage = new Stage(scenarioInScene);
+            scenarioInScene.StartCoroutine(Cleanup());
+
+            IEnumerator Cleanup()
+            {
+                while (stage.IsClosed == false)
+                    yield return null;
+                Destroy(scenarioInScene);
             }
         }
 
-        public class ScenarioTesterComp : ScenarioInScene
-        {
+        public class ScenarioTesterComp : ScenarioInScene { }
 
+        class DoNothing : ICommand
+        {
+            public void ValidateSelf(){}
+
+            public IEnumerable<(string name, IValidatable validatable)> GetSubValues() => this.NoSubValues();
+
+            public string GetInspectorString() => "Do Nothing";
+
+            public IEnumerable Run(Stage stage)
+            {
+                yield break;
+            }
         }
     }
 }
