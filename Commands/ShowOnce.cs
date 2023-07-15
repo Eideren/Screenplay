7fd80c022d17d3f944f2e37500e194b5cdf567f3
Eideren <contact@eideren.com>
2023-07-15 22:51:55 +0200

    Commands: ShowOnce

---
+++ Commands/ShowOnce.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Screenplay.Commands
+{
+    [Serializable] public class ShowOnce : IShowOnCondition
+    {
+        readonly HashSet<Screech.Node> _visited = new();
+
+        public bool Show(Stage stage, Screech.Node line)
+        {
+            // This object instance might be used across multiple lines,
+            // use a collection to store whether it visited each lines
+            return _visited.Add(line);
+        }
+
+        public void ValidateSelf(){ }
+
+        public IEnumerable<(string name, IValidatable validatable)> GetSubValues() => this.NoSubValues();
+
+        public string GetInspectorString() => "Show this line only once";
+    }
+}
