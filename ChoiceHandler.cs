88e25db1a183a33fe1641d6973aae94423eaf469
Eideren <contact@eideren.com>
2023-08-28 03:26:22 +0200

    Component: TextFeed and ChoiceHandler component to customize UI

---
+++ ChoiceHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Screenplay
+{
+    public class ChoiceHandler : MonoBehaviour
+    {
+        public Button Prototype;
+        public UnityEvent OnPresentChoices;
+        public UnityEvent<Button> OnSelectedChoice;
+        Stage _stage;
+
+        void OnValidate()
+        {
+            if (Prototype == null)
+                Debug.Log($"{nameof(Prototype)} is null", this);
+            else if (Prototype.GetComponentInChildren<TMP_Text>() == null)
+                Debug.Log($"No TextMeshPro Text component set within {nameof(Prototype)} '{Prototype}'", this);
+        }
+
+        void OnEnable()
+        {
+            Prototype.gameObject.SetActive(false);
+            if (_stage == null)
+                Stage.UIChoiceHandler += UIChoiceHandler;
+        }
+
+        void OnDisable()
+        {
+            if (_stage == null)
+                Stage.UIChoiceHandler -= UIChoiceHandler;
+        }
+
+        void UIChoiceHandler(Stage stage, int choices, out TMP_Text[] choiceTextComp, out Task<TMP_Text> waitingForChoice)
+        {
+            Stage.UIChoiceHandler -= UIChoiceHandler;
+            _stage = stage;
+            var buttons = new Button[choices];
+            var completion = new TaskCompletionSource<TMP_Text>();
+            waitingForChoice = completion.Task;
+            choiceTextComp = new TMP_Text[choices];
+            for (int i = 0; i < choices; i++)
+            {
+                var button = buttons[i] = Instantiate(Prototype.gameObject).GetComponent<Button>();
+                button.transform.SetParent(Prototype.transform.parent);
+                button.gameObject.SetActive(true);
+                var text = choiceTextComp[i] = buttons[i].GetComponentInChildren<TMP_Text>();
+                button.onClick.AddListener(delegate
+                {
+                    _stage = null;
+                    Stage.UIChoiceHandler += UIChoiceHandler;
+                    completion.SetResult(text);
+                    foreach (var otherButton in buttons)
+                        Destroy(otherButton.gameObject);
+                    OnSelectedChoice?.Invoke(button);
+                });
+            }
+            OnPresentChoices?.Invoke();
+        }
+    }
+}
---
+++ TextFeed.cs
@@ -0,0 +1,86 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Screenplay
+{
+    public class TextFeed : MonoBehaviour
+    {
+        public TMP_Text Text;
+        public UnityEvent OnOpen, OnClose, OnSwappingLine, OnTypedAllCharacters, OnTypingCharacter;
+        public Stage CurrentStage;
+        bool _typedAll;
+        int _visibleChars;
+
+        void OnEnable()
+        {
+            Stage.UIGetTextFeed += ProvideTextFeed;
+        }
+
+        void OnDisable()
+        {
+            Stage.UIGetTextFeed -= ProvideTextFeed;
+        }
+
+        TMP_Text ProvideTextFeed(Stage newStage)
+        {
+            if (CurrentStage != null && newStage == CurrentStage)
+            {
+                // The same stage is displaying a new line and so requests a text feed,
+                // we could provide another one if we need to display multiple bubbles for different interlocutors.
+                // In this case we won't, just re-use the same text feed for that new line.
+                return Text;
+            }
+
+            CurrentStage = newStage;
+            OnStageOpened();
+            Stage.OnClose += OnCloseAny;
+            return Text;
+
+            void OnCloseAny(Stage closingStage)
+            {
+                if (closingStage == CurrentStage)
+                {
+                    OnStageClosed();
+                    CurrentStage = null;
+                    Stage.OnClose -= OnCloseAny;
+                }
+            }
+        }
+
+        void OnStageOpened()
+        {
+            CurrentStage.WhileOpen += TickStage;
+            CurrentStage.OnDoneWithLine += OnSwappingLine.Invoke;
+            OnOpen?.Invoke();
+        }
+
+        void OnStageClosed()
+        {
+            CurrentStage.WhileOpen -= TickStage;
+            CurrentStage.OnDoneWithLine -= OnSwappingLine.Invoke;
+            OnClose?.Invoke();
+        }
+
+        void TickStage()
+        {
+            if (_visibleChars == Text.maxVisibleCharacters)
+                return;
+
+            _visibleChars = Text.maxVisibleCharacters;
+
+            OnTypingCharacter.Invoke();
+
+            bool done = Stage.CountWithoutTags(Text.text, Text.text.Length) == Text.maxVisibleCharacters;
+            if (done && _typedAll == false)
+            {
+                _typedAll = true;
+                OnTypedAllCharacters?.Invoke();
+            }
+            else if (done == false)
+            {
+                _typedAll = false;
+            }
+        }
+    }
+}
