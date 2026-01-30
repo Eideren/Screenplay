a358362dfa603926667ef50c50196a9946f30afd
Eideren <contact@eideren.com>
2026-01-30 14:22:00 +0100

    feat: WithInterruptingCancellation extension

---
+++ UniTaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+
+public static class UniTaskExtensions
+{
+    public static async UniTask WithInterruptingCancellation(this UniTask task, CancellationToken cancellationToken)
+    {
+        await task.AttachExternalCancellation(cancellationToken);
+        cancellationToken.ThrowIfCancellationRequested();
+    }
+
+    public static async UniTask<T> WithInterruptingCancellation<T>(this UniTask<T> task, CancellationToken cancellationToken)
+    {
+        var val = await task.AttachExternalCancellation(cancellationToken);
+        cancellationToken.ThrowIfCancellationRequested();
+        return val;
+    }
+}
